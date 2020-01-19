fc7580e36823e5210852fadce2bf6810e89984ca
Jon Bash <jonbash@protonmail.com>
2020-01-18 16:04:13 -0800

    init xcode, organize

---
+++ Planets-Obj-c/Planets-Obj-c/Application/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Planets-Obj-c
+//
+//  Created by Jon Bash on 2020-01-18.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Planets-Obj-c/Planets-Obj-c/Application/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Planets-Obj-c
+//
+//  Created by Jon Bash on 2020-01-18.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
