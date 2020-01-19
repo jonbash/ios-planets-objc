ad9be06a90773807c62ed6bd486e8e324f03d6de
Jon Bash <jonbash@protonmail.com>
2020-01-18 16:54:46 -0800

    add planetscontroller

--- Planets-Obj-c/Planets-Obj-c/Model Controllers/JBPlanetsController.h
+++ Planets-Obj-c/Planets-Obj-c/Model Controllers/JBPlanetsController.h
@@ -7,10 +7,14 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "JBPlanet.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface JBPlanetsController : NSObject
+
+@property NSArray *planets;
+@property JBPlanet *pluto;
 
 @end
 
--- Planets-Obj-c/Planets-Obj-c/Model Controllers/JBPlanetsController.m
+++ Planets-Obj-c/Planets-Obj-c/Model Controllers/JBPlanetsController.m
@@ -6,8 +6,39 @@
 //  Copyright © 2020 Jon Bash. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
 #import "JBPlanetsController.h"
+#import "JBPlanet.h"
 
 @implementation JBPlanetsController
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _planets = [[NSArray alloc] init];
+        _planets = [[NSArray alloc] initWithObjects:
+                    [[JBPlanet alloc] initWithName:@"Mercury"
+                                             image:[UIImage imageNamed:@"mercury.png"]],
+                    [[JBPlanet alloc] initWithName:@"Venus"
+                                             image:[UIImage imageNamed:@"venus.png"]],
+                    [[JBPlanet alloc] initWithName:@"Earth"
+                                             image:[UIImage imageNamed:@"earth.png"]],
+                    [[JBPlanet alloc] initWithName:@"Mars"
+                                             image:[UIImage imageNamed:@"mars.png"]],
+                    [[JBPlanet alloc] initWithName:@"Jupiter"
+                                             image:[UIImage imageNamed:@"jupiter.png"]],
+                    [[JBPlanet alloc] initWithName:@"Saturn"
+                                             image:[UIImage imageNamed:@"saturn.png"]],
+                    [[JBPlanet alloc] initWithName:@"Uranus"
+                                             image:[UIImage imageNamed:@"uranus.png"]],
+                    [[JBPlanet alloc] initWithName:@"Neptune"
+                                             image:[UIImage imageNamed:@"neptune.png"]],
+                    nil];
+        _pluto = [[JBPlanet alloc] initWithName:@"Pluto"
+                                          image:[UIImage imageNamed:@"pluto.png"]];
+    }
+    return self;
+}
 
 @end
